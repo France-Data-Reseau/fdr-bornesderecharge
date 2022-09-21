b24c982dfa30f1c2c9b3fc2ae3248eff2a649829
Marc Dutoo <marc.dutoo@gmail.com>
2022-09-21 19:35:47 +0200

    remove translated_macro (useless anyway)

--- models/sdirve/src/sdirve_src_indicateurs_parsed.sql
+++ models/sdirve/src/sdirve_src_indicateurs_parsed.sql
@@ -14,5 +14,4 @@ Generically parsed
 {{ fdr_appuiscommuns.fdr_source_union_from_name(FDR_SOURCE_NOM,
     has_dictionnaire_champs_valeurs,
     this,
-    translated_macro=sdirve_indicateurs_translated,
     def_model=ref('sdirve_def_indicateurs_definition')) }}
